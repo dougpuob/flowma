59c3b310f90f24783b70ff5a369e77e277b9684b
Douglas Chen <dougpuob@gmail.com>
2022-03-28 01:28:05 +0800

    WIP

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 ### Dependency
 - pip install toml
 
+### Config
+.ci/flowma.toml
+.gitlab/flowma.toml
+_ci/flowma.toml
+_gitlab/flowma.toml
+config/flowma.toml
 
--- flowma.py
+++ flowma.py
@@ -10,29 +10,10 @@ from src.cmdparser import cmdparser
 
 class flowma():
 
+
     def __init__(self):
         self.cmd = cmdparser()
         self.args = cmd.get_parsed_args()
-
-        # logger = logging.getLogger()
-        # logger.setLevel(logging.INFO)
-        # formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s',
-        # 	                            datefmt='%Y%m%d %H:%M:%S')
-        # screen = logging.StreamHandler()
-        # screen.setLevel(logging.INFO)
-        # screen.setFormatter(formatter)
-
-        # logdir='data/log'
-        # if not os.path.exists(logdir):
-        #     os.makedirs(logdir)
-
-        # filename = datetime.datetime.now().strftime("qemu-tasker--%Y%m%d_%H%M%S.log")
-        # logfile = logging.FileHandler(os.path.join(logdir, filename))
-        # logfile.setLevel(logging.INFO)
-        # logfile.setFormatter(formatter)
-
-        # logger.addHandler(logfile)
-        # socket_addr = config_next.socket_address(args.host, args.port)
 
 
     def main():
