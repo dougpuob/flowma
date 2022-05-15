e71ee43b49a7dc87bd33063f45c9f61679a605c4
Douglas Chen <dougpuob@gmail.com>
2022-05-15 22:40:48 +0800

    WIP

---
+++ doc/plan.mm
@@ -0,0 +1,148 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Plan" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1652625397243">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font NAME="Tahoma" BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="gitlab" POSITION="right" ID="ID_22397888" CREATED="1652624810840" MODIFIED="1652625397249">
+<font NAME="Tahoma"/>
+<node TEXT="executors" ID="ID_1718031656" CREATED="1652625031040" MODIFIED="1652625397252">
+<font NAME="Tahoma"/>
+<node TEXT="docker" ID="ID_1623337708" CREATED="1652624887846" MODIFIED="1652625397254">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="shell" ID="ID_782436739" CREATED="1652624996388" MODIFIED="1652625397255">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="virtualbox" ID="ID_1097389260" CREATED="1652625000165" MODIFIED="1652625397228">
+<font NAME="Tahoma"/>
+</node>
+</node>
+</node>
+<node TEXT="pve" POSITION="right" ID="ID_1148456030" CREATED="1652624815265" MODIFIED="1652625397256">
+<font NAME="Tahoma"/>
+<node TEXT="Linux Container" ID="ID_1305621740" CREATED="1652624832212" MODIFIED="1652625397257">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="Windows Container" ID="ID_685922577" CREATED="1652624826885" MODIFIED="1652625397259">
+<font NAME="Tahoma"/>
+</node>
+</node>
+<node TEXT="flowma" POSITION="right" ID="ID_1773659692" CREATED="1652624818607" MODIFIED="1652625397260">
+<font NAME="Tahoma"/>
+<node TEXT="introduction" ID="ID_1902281232" CREATED="1652625087157" MODIFIED="1652625397261">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="linter" ID="ID_1542098393" CREATED="1652625286029" MODIFIED="1652625397261">
+<font NAME="Tahoma"/>
+<node TEXT="clang-format" ID="ID_112259040" CREATED="1652625289044" MODIFIED="1652625397262">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="clang-tidy" ID="ID_109042808" CREATED="1652625293020" MODIFIED="1652625397263">
+<font NAME="Tahoma"/>
+</node>
+</node>
+<node TEXT="build system" ID="ID_725504213" CREATED="1652625300233" MODIFIED="1652625397264">
+<font NAME="Tahoma"/>
+<node TEXT="msbuild" ID="ID_1665503891" CREATED="1652625303996" MODIFIED="1652625397265">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="ninja" ID="ID_1552131194" CREATED="1652625306455" MODIFIED="1652625397265">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="makefiles" ID="ID_832934527" CREATED="1652625308065" MODIFIED="1652625397265">
+<font NAME="Tahoma"/>
+</node>
+<node TEXT="xcode" ID="ID_1942939093" CREATED="1652625330031" MODIFIED="1652625397265">
+<font NAME="Tahoma"/>
+</node>
+</node>
+<node TEXT="languages" ID="ID_1142519625" CREATED="1652625319613" MODIFIED="1652625412246">
+<font NAME="Tahoma"/>
+<node TEXT="c/c++" ID="ID_1644181785" CREATED="1652625413042" MODIFIED="1652625418337"/>
+<node TEXT="python" ID="ID_1057381455" CREATED="1652625418650" MODIFIED="1652625420423"/>
+<node TEXT="rust" ID="ID_992039643" CREATED="1652625420631" MODIFIED="1652625425766"/>
+</node>
+<node TEXT="package" ID="ID_142746542" CREATED="1652625503453" MODIFIED="1652625552226">
+<node TEXT="conan" ID="ID_1616607329" CREATED="1652625554885" MODIFIED="1652625558529"/>
+</node>
+<node TEXT="testing" ID="ID_1459912515" CREATED="1652625589590" MODIFIED="1652625592058"/>
+<node TEXT="report" ID="ID_813786732" CREATED="1652625574991" MODIFIED="1652625580825"/>
+</node>
+</node>
+</map>
